228244c75c09be6e473da001532e752223c4ea03
wangjinsheng <wangjinsheng593@163.com>
2022-02-09 09:08:21 +0800

    feat(配置): feat：i18n

--- src/main.js
+++ src/main.js
@@ -13,7 +13,11 @@ import installIcons from '@/icons';
 // 导入路由鉴权
 import './permission';
 
+// i18n （PS：导入放到 APP.vue 导入之前，因为后面我们会在 app.vue 中使用国际化内容）
+import i18n from '@/i18n';
+
 const app = createApp(App);
 installElementPlus(app);
 installIcons(app);
+app.use(i18n);
 app.use(store).use(router).mount('#app');
