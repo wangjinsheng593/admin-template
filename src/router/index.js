65b4bd5004eb1b8b24ebd30bf615507114758fef
wangjinsheng593 <wangjinsheng593@163.com>
2022-02-14 20:32:51 +0800

    fix：eslin校验修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,143 +1,141 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import {
+    createRouter,
+    createWebHashHistory
+} from 'vue-router';
 import layout from '@/layout/index';
 
 //  const routes = []
 /**
  * 公开路由表
  */
-const publicRoutes = [
-  {
+const publicRoutes = [{
     path: '/login',
     component: () => import('@/views/login/index')
-  },
-  {
+},
+{
     path: '/',
     redirect: '/profile',
     component: layout,
-    children: [
-      {
+    children: [{
         path: '/profile',
         name: 'profile',
         component: () => import('@/views/profile/index'),
         meta: {
-          title: '个人中心',
-          icon: 'el-icon-user'
+            title: '个人中心',
+            icon: 'el-icon-user'
         }
-      },
-      {
+    },
+    {
         path: '/404',
         name: '404',
         component: () => import('@/views/error-page/404')
-      },
-      {
+    },
+    {
         path: '/401',
         name: '401',
         component: () => import('@/views/error-page/401')
-      }
+    }
     ]
-  }
+}
 ];
 
 /**
  * 私有路由表
  */
-const privateRoutes = [
-  {
+const privateRoutes = [{
     path: '/user',
     component: layout,
     redirect: '/user/manage',
     meta: {
-      title: '用户',
-      icon: 'personnel'
+        title: '用户',
+        icon: 'personnel'
     },
-    children: [
-      {
+    children: [{
         path: '/user/manage',
         component: () => import('@/views/user-manage/index'),
         meta: {
-          title: '用户管理',
-          icon: 'personnel-manage'
+            title: '用户管理',
+            icon: 'personnel-manage'
         }
-      },
-      {
+    },
+    {
         path: '/user/role',
         component: () => import('@/views/role-list/index'),
         meta: {
-          title: '角色列表',
-          icon: 'role'
+            title: '角色列表',
+            icon: 'role'
         }
-      },
-      {
+    },
+    {
         path: '/user/permission',
         component: () => import('@/views/permission-list/index'),
         meta: {
-          title: '权限列表',
-          icon: 'permission'
+            title: '权限列表',
+            icon: 'permission'
         }
-      },
-      {
+    },
+    {
         path: '/user/info/:id',
         name: 'userInfo',
         component: () => import('@/views/user-info/index'),
         meta: {
-          title: '员工管理'
+            title: '员工管理'
         }
-      },
-      {
+    },
+    {
         path: '/user/import',
         name: 'import',
         component: () => import('@/views/import/index'),
         meta: {
-          title: '导出'
+            title: '导出'
         }
-      }
+    }
     ]
-  },
-  {
+},
+{
     path: '/article',
     component: layout,
     redirect: '/article/ranking',
     meta: {
-      title: '文章',
-      icon: 'article'
+        title: '文章',
+        icon: 'article'
     },
-    children: [
-      {
+    children: [{
         path: '/article/ranking',
         component: () => import('@/views/article-ranking/index'),
         meta: {
-          title: '文章排名',
-          icon: 'article-ranking'
+            title: '文章排名',
+            icon: 'article-ranking'
         }
-      },
-      {
+    },
+    {
         path: '/article/:id',
         component: () => import('@/views/article-detail/index'),
         meta: {
-          title: '文章详情'
+            title: '文章详情'
         }
-      },
-      {
+    },
+    {
         path: '/article/create',
         component: () => import('@/views/article-create/index'),
         meta: {
-          title: '创建文章',
-          icon: 'article-create'
+            title: '创建文章',
+            icon: 'article-create'
         }
-      },
-      {
+    },
+    {
         path: '/article/editor/:id',
         component: () => import('@/views/article-create/index'),
         meta: {
-          title: '编辑文章'
+            title: '编辑文章'
         }
-      }
+    }
     ]
-  }
+}
 ];
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [...publicRoutes, ...privateRoutes]
+    history: createWebHashHistory(),
+    routes: [...publicRoutes, ...privateRoutes]
 });
 
 export default router;
