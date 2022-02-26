8d528c113fd27f4e3bf48c95cdc5207cfee8003a
wangjinsheng593 <wangjinsheng593@163.com>
2022-02-26 15:48:45 +0800

    feat(element-plus 升级本版): element-plus 本版从1.0.2升级到2.0.3版本

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,12 +1,12 @@
 import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/dist/index.css';
 // import locale from 'element-plus/lib/locale/lang/zh-cn';
 import store from '@/store';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import en from 'element-plus/lib/locale/lang/en';
 
-export default (app) => {
-    app.use(ElementPlus, {
-        locale: store.getters.language === 'en' ? en : zhCn
-    });
+export default app => {
+	app.use(ElementPlus, {
+		locale: store.getters.language === 'en' ? en : zhCn,
+	});
 };
