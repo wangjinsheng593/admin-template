cbf75421aa0cbf2751d26064306aba52766b180e
wangjinsheng593 <wangjinsheng593@163.com>
2022-03-20 17:24:45 +0800

    feat(动态表格): 动态表格列表完成

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs';
-
+import rt from 'dayjs/plugin/relativeTime';
+// 语言包
+import 'dayjs/locale/zh-cn';
+import store from '@/store';
 /**
  * 时间戳格式化
  * @param {*} val
@@ -14,8 +17,20 @@ const dateFilter = (val, format = 'YYYY-MM-DD') => {
 	return dayjs(val).format(format);
 };
 
+// 加载相对时间插件
+dayjs.extend(rt);
+function relativeTime(val) {
+	if (!isNaN(val)) {
+		val = parseInt(val);
+	}
+	return dayjs()
+		.locale(store.getters.language === 'zh' ? 'zh-cn' : 'en')
+		.to(dayjs(val));
+}
+
 export default app => {
 	app.config.globalProperties.$filters = {
 		dateFilter,
+		relativeTime,
 	};
 };
